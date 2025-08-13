206f38eccf93b0cd31e0a3792e2b3a1171b26c03
jeronimo22521 <jeronimo.cuartas@upb.edu.co>
2025-08-13 14:48:48 -0500

    Add files via upload

---
+++ 3.Condicionales/3.Condicionales/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _3.Condicionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONDICIONAL SIMPLE
+            string nombre = "";
+            int sueldo = 0;
+
+            Console.WriteLine("ingrese el nombre");
+            nombre = Console.ReadLine();//Todo lo que yo leo desde la consola llega como string
+            Console.WriteLine(nombre);
+            Console.WriteLine("ingrese el sueldo");
+            sueldo = Convert.ToInt32(Console.ReadLine());
+
+            if (sueldo > 3000)
+            {
+                Console.WriteLine($"La persona {nombre}, debe abonar impuestos");
+            }
+
+            //Crea un adgoritmo que lea la edad de un usuario, si el usuarioes mayor de 18 se debe mostrar el siguiente mensaje; "bienvenido al sitio web" /
+
+            int edad = 0;
+            Console.WriteLine("ingrese su edad");
+
+            edad = int.Parse(Console.ReadLine());
+
+            if (edad > 18)
+            {
+                Console.WriteLine("bienvenido al sitio web");
+            }
+            else
+            {
+                Console.WriteLine("acceso denegado, debes ser mayor de 18 para acceder al sitio web");
+            }
+        }
+        
+
+
+       
+        
+            
+      
+    }
+}
