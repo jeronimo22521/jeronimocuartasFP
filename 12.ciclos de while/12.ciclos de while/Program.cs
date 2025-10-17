3bc20b5bde04829db4413b4c854c30f729293032
jeronimo22521 <jeronimo.cuartas@upb.edu.co>
2025-10-17 14:54:18 -0500

    Add files via upload

---
+++ 12.ciclos de while/12.ciclos de while/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _12.ciclos_de_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Realizar algoritmo que permita obtener la suma de todos los números impares hasta el 99. con while contador y acumulador
+
+            int contador = 1;
+            int acumulador = 0;
+            while (contador < 100)
+            {
+                acumulador += contador;
+                contador += 2;
+            }
+
+            Console.WriteLine("La suma de todos los números impares hasta el 99 es: " + acumulador);
+
+
+
+        }
+    }
+}
