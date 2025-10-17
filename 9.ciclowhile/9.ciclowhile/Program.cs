8009d300b2de579706e2df417dda4ac911caa4bf
jeronimo22521 <jeronimo.cuartas@upb.edu.co>
2025-10-17 15:14:30 -0500

    Add files via upload

---
+++ 9.ciclowhile/9.ciclowhile/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _9.ciclowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribir un algoritmo que permita generar primeros 5 números enteros positivos y realizar y mostrar su suma.
+            //con contador y acumulador
+
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <= 5)
+            {
+                acumulador += contador;//acumulador = acumulador + contador
+                Console.WriteLine($"C: {contador} - A: {acumulador}"); //es para ver el proceso
+                contador++;
+            }
+
+
+            Console.WriteLine($"La suma de los primeros 5 números enteros positivos es: {acumulador}");
+
+        }
+    }
+}
