da9427252677c2ad9183ce0f0e311f225c96c155
jeronimo22521 <jeronimo.cuartas@upb.edu.co>
2025-10-17 15:01:34 -0500

    Add files via upload

---
+++ 21/21/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _21_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // ciclo for
+
+            int acumulator = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                acumulator += i;
+
+            }
+
+            Console.WriteLine($"La suma de los primeros cinco numeros es: {acumulator}");
+        }
+    }
+}
