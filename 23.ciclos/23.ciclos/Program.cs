3d2f1778f6216a5854fd4120ad9b59ca38c8c7c4
jeronimo22521 <jeronimo.cuartas@upb.edu.co>
2025-10-17 15:07:05 -0500

    Add files via upload

---
+++ 23.ciclos/23.ciclos/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _23.ciclos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear arreglo llamado numeros de 100 elementos y asignar el numero 10 en cada una
+            //de las posiciones del arreglo utilizando un ciclo for, ademas leer el contenido de cada elemento y
+            //escribirlo en pantalla
+            int[] numeros = new int[100];
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = 10;
+            }
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
