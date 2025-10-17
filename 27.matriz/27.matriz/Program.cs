9a6836841b237d93da14f4eb89de2100013f2ff4
jeronimo22521 <jeronimo.cuartas@upb.edu.co>
2025-10-17 15:11:22 -0500

    Add files via upload

---
+++ 27.matriz/27.matriz/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _27.matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desarrolla una aplicación que realice las siguientes tareas: 
+            //Solicita al usuario que ingrese las dimensiones de una matriz(número de filas y columnas). 
+            //Recorre la matriz y permite al usuario ingresar un número entero en cada posición. 
+            //Una vez cargada la matriz, el programa debe contar cuántos de los números ingresados son positivos, cuántos son negativos y cuántos son ceros.
+            //Finalmente, muestra por pantalla los totales obtenidos para cada categoría(positivos, negativos y ceros).
+
+            Console.WriteLine("Ingrese el número de filas de la matriz:");
+            int filas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el número de columnas de la matriz:");
+            int columnas = int.Parse(Console.ReadLine());
+            int[,] matriz = new int[filas, columnas];
+            int positivos = 0, negativos = 0, ceros = 0;
+            for (int i = 0; i < filas; i++)
+            {
+                for (int j = 0; j < columnas; j++)
+                {
+                    Console.WriteLine($"Ingrese el número para la posición [{i},{j}]:");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                    if (matriz[i, j] > 0)
+                    {
+                        positivos++;
+                    }
+                    else if (matriz[i, j] < 0)
+                    {
+
+
+
+
+
+
+
+
+
+                        negativos++;
+                    }
+                    else
+                    {
+                        ceros++;
+                    }
+                }
+            }
+            Console.WriteLine($"Total de números positivos: {positivos}");
+            Console.WriteLine($"Total de números negativos: {negativos}");
+            Console.WriteLine($"Total de ceros: {ceros}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ 28.programacion modular/28.programacion modular/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _28.programacion_modular
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                MostrarMensaje("Soy Valeria");
+                MostrarMensaje("Tengo 44 años");
+                MostrarMensaje("Adiós Mundo");
+            }
+
+            static void MostrarMensaje(string mensaje)
+            {
+                Console.WriteLine("Bienvenido a la programación modular");
+                MostrarMensaje("Estoy siendo llamado desde MostrarMensaje");
+            }
+
+            static int CalcularEdad() //todos los modulos deben ser estaticos 
+            {
+                int Edad = 2025 - 1981;
+                return Edad;
+            }
+
+            static int calcularEdad2(int añoActual, int añoNacimiento) //modulo con parametros sirve para reutilizar el codigo
+
+            {
+                return añoActual - añoNacimiento;
+            }
+        }
+    }
+}
